779aa0d7aee43d8b8c1139281aa1ad8a66065c8f
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-03-23 15:44:06 -0700

    Added code to hide the clear and filter button based on the list being empty and added a prompt for the user when deleting items. Additional comments have been added to describe the code.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 // Function to create a new icon and set custom classes
 function createIcon(classes) {
@@ -56,29 +57,64 @@ function addItem (e) {
     // Append new item to item list
     itemList.appendChild(li);
 
+    // Runs checkUI Function
+    checkUI();
+
     // Sets itemInput to empty string
     itemInput.value = '';
 };
 
 // Function to remove items from the list
 function removeItem(e) {
-    // Checks if element being hit contains the correct class
+    // Prompts the user to confirm the list deletion
     if (e.target.parentElement.classList.contains('remove-item')) {
-        //Removes the parent of the button's parent element, or the whole list item
-        e.target.parentElement.parentElement.remove();
+        // Checks if element being hit contains the correct class
+        if (confirm('Are you sure?')) {
+            //Removes the parent of the button's parent element, or the whole list item
+            e.target.parentElement.parentElement.remove();
+        }
     }
+
+    // Runs checkUI Function
+    checkUI();
 };
 
 // Function to clear all items from the list
 function clearItems() {
-    // Iterates through the whole list so long as it has a child element
-    while (itemList.firstChild) {
-        // Removes the child element of the item list
-        itemList.removeChild(itemList.firstChild);
-    };
+    // Prompts the user to confirm the list deletion
+    if (confirm('Warning: All item will be deleted.')) {
+        // Iterates through the whole list so long as it has a child element
+        while (itemList.firstChild) {
+            // Removes the child element of the item list
+            itemList.removeChild(itemList.firstChild);
+        };
+    }
+
+    // Runs checkUI Function
+    checkUI();
+};
+
+// Function to check the state of certain UI features
+function checkUI () {
+    // Initializes a variable with a value of all items in the list
+    const items = itemList.querySelectorAll('li');
+
+    // Check if the list has items in it.
+    if (items.length === 0) {
+        // Hides the clear button and item filter search bar if list is empty
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        // Makes the clear button and item filter visible if the list is populated
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
 };
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+
+// Runs the checkUI function upon loading the webpage
+checkUI();
